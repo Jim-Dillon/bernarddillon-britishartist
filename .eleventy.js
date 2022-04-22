624ff7b68647414380bc3699d810d5904f6a9de2
Jim-Dillon <james.dillon94@gmail.com>
2022-04-22 16:18:57 +0100

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addPassthroughCopy("src/css");
+    return {
+        dir: {
+            input: "src",
+            layouts: "_includes",
+        },
+    };
+};    
