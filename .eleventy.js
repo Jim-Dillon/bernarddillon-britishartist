8178d763dfa5407bc577ecaabf95eb4454e35211
Jim-Dillon <james.dillon94@gmail.com>
2022-04-28 14:54:27 +0100

    add homepage gallery carousel

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("src/js/carousel.js")
     eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.addPassthroughCopy("src/css");
     return {
--- src/js/carousel.js
+++
@@ -1,3 +0,0 @@
-slides1 = 10;
-
-console.log(slides1);
