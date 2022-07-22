dadd6bb0a6f2b82306e6183a952751fc1592a45c
Jim-Dillon <Jim-Dillon@users.noreply.github.com>
2022-07-22 14:09:13 +0000

    deploy: 041838af9f5008fc1c2cc35b8c86bc89c5abf54d

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,16 +1,36 @@
+window.addEventListener('load', (event) => {
+
+// const header = document.getElementsByClassName('dropdown-header');
+
+
+
 const addDropdown = (e) => {
     e.preventDefault();
-    const header = document.getElementsByClassName('dropdown-header');
+    const header = document.querySelector('.dropdown-header');
+    console.log(header);
     if (header) {
-        header.classList.add('open');
-    }
+        header.classList.add('open');       
+    }  
 }
+
+const openButton = document.querySelector(".dropdown-icon");
+openButton.addEventListener("click", addDropdown);
 
 
 const removeDropdown = (e) => {
     e.preventDefault();
-    const header = document.getElementsByClassName('dropdown-header');
+    const header = document.querySelector('.dropdown-header');
+    console.log(header);
     if (header) {
         header.classList.remove('open');
     }
 }
+
+
+
+const closeButton = document.querySelector(".x-icon");
+closeButton.addEventListener("click", removeDropdown);
+
+
+
+});
